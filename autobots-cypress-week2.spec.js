b20f6e1f8e112fe2cb86ec170e0b149ff58a5060
SadeeshKumarMN <sadeeshkumarmn@gmail.com>
2020-02-18 21:27:50 +0530

    Autobots-Cypress Week2 Challenges

---
+++ autobots-cypress-week2.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('autobots-cypress week2 challenge', ()=> {
+
+    beforeEach(() => {
+        cy.visit('https://www.qap.dev/')
+      })
+
+    it('Click the Become a presenter button',() => {
+        cy.contains('Become a').click()
+        cy.url().should('eq', 'https://www.qap.dev/present-at-qap')
+    })
+    
+    it('Verify the name \'Kidman\' in the leadership page',() => {  
+        cy.get('.Footer-nav > :nth-child(1) > [href="/leadership"]').click()
+        cy.get('.Main-content').should('contain.text', 'Carlos Kidman')
+        cy.get('strong:contains("Carlos Kidman")').should('have.length', 2)
+    })
+
+})
+
